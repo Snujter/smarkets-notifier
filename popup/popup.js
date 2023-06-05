3f4eba19a204605ef401ba247a9d64e1c7461726
Snajder <snaaajder@gmail.com>
2023-06-06 01:32:42 +0200

    Add Contract template for popup

--- popup/popup.js
+++ popup/popup.js
@@ -68,14 +68,63 @@ class MarketElement extends HTMLElement {
         const slotElement = this.shadowRoot.querySelector(`slot[name="${name}"]`);
         slotElement.textContent = value;
     }
+
+    addContract(name, value, status = "inactive") {
+        const contractsSlot = this.shadowRoot.querySelector('slot[name="contracts"]');
+        const contractElement = document.createElement("contract-element");
+        contractElement.setAttribute("name", name);
+        contractElement.setAttribute("value", value);
+        contractElement.setAttribute("status", status);
+        contractsSlot.append(contractElement);
+        return contractElement;
+    }
+}
+
+class ContractElement extends HTMLElement {
+    static TEMPLATE_ID = "contract-template";
+
+    static get observedAttributes() {
+        return ["name", "value", "status"];
+    }
+
+    constructor() {
+        super();
+        const template = document.getElementById(ContractElement.TEMPLATE_ID);
+        const templateContent = template.content.cloneNode(true);
+
+        const shadowRoot = this.attachShadow({ mode: "open" });
+        shadowRoot.appendChild(templateContent.cloneNode(true));
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (oldValue !== newValue) {
+            this.updateSlotContent(name, newValue);
+        }
+    }
+
+    updateSlotContent(name, value) {
+        const slotElement = this.shadowRoot.querySelector(`slot[name="${name}"]`);
+
+        if (name == "status") {
+            const $container = this.shadowRoot.querySelector(`.container`);
+            slotElement.classList.toggle("active", value === "active");
+            slotElement.classList.toggle("inactive", value === "inactive");
+            $container.classList.toggle("inactive", value === "inactive");
+            slotElement.classList.toggle("warning", value === "warning");
+            $container.classList.toggle("warning", value === "warning");
+        } else {
+            slotElement.textContent = value;
+        }
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
     // Load templates to DOM
-    loadTemplates(["event", "market"]).then(function () {
+    loadTemplates(["event", "market", "contract"]).then(function () {
         // Create custom elements from templates
         customElements.define("event-element", EventElement);
         customElements.define("market-element", MarketElement);
+        customElements.define("contract-element", ContractElement);
     });
 });
 
